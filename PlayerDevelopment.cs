1cc6cb3e523f4681cb0a3a2b1e7c116d519f0e8e
alokyadavnp <61194564+alokyadavnp@users.noreply.github.com>
2020-02-18 16:20:58 +0200

    delete

--- PlayerDevelopment.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerDevelopment // this is not a monobehaviour but a pure data class.
-{
-    public int TigerhighestScore; // to store the highest score tiger round has achieved.
-    public int GoathighestScore;  // to store the highest score goat round has achieved.
-}
